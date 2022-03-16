81a7cfcb3c65264ef3e0365e9cf547dc9568acdf
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-03-16 13:59:43 +0530

    Update Fibonacci.java

--- Dynamic-Programming/Fibonacci.java
+++ Dynamic-Programming/Fibonacci.java
@@ -1,1 +1,22 @@
+//   LeetCode509 - Fibonacci Number
 
+pubic int fib(int n) {
+        return checkFib(n, new HashMap<Integer, Integer>());
+    }
+    
+    public int checkFib(int n, HashMap<Integer, Integer> memo)
+    {
+        if(n ==1)return 1;
+        
+         if(n ==0)return 0;
+        
+        int currentKey = n;
+        
+        if(memo.containsKey(n)) return memo.get(n);
+        
+        int a = checkFib(n-1, memo);
+        int b = checkFib(n-2, memo);
+        
+        memo.put(currentKey, a+b);
+        return memo.get(currentKey);
+    }
