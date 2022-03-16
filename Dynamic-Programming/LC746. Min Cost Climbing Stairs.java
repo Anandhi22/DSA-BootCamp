bd7708662b6ee12b16bfd70bbff121da4f14a96a
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-03-16 15:37:11 +0530

    easy understanding dp

--- Dynamic-Programming/LC746. Min Cost Climbing Stairs.java
+++ Dynamic-Programming/LC746. Min Cost Climbing Stairs.java
@@ -1,16 +1,35 @@
-    public  int minCostClimbingStairs(int[] cost) {
+ public  int minCostClimbingStairs(int[] cost) {
 
+        
+        //When you strat from Zeroth Index
         int indexZero = findClimbs(0, cost, new HashMap<>());
+        
+        //When you strat from Zeroth Index
         int indexOne = findClimbs(1, cost, new HashMap<>());
+        
+        //minimum of both
         return Math.min(indexOne, indexZero);
      }
 
     public  int findClimbs(int currIndex, int[] cost, HashMap<Integer, Integer> memo) {
-        if (currIndex == cost.length) return  + 0;
-        else if (currIndex > cost.length) return + 1001;
+        //when you found a path return 0
+        if (currIndex == cost.length) return  0;
+        
+        //when you hav over jumped the last stair, return 1001
+        else if (currIndex > cost.length) return  1001;
+        
+        //registering in map, hence dp 
         if (memo.containsKey(currIndex)) return memo.get(currIndex);
+        
+        //one Jump 
         int oneJump = cost[currIndex]+ findClimbs(currIndex + 1, cost, memo);
+        
+        //two jump
         int twoJumps = cost[currIndex]+ findClimbs(currIndex + 2, cost, memo);
+        
+        //update the map
         memo.put(currIndex, Math.min(oneJump, twoJumps));
+        
+        //return the total cost
         return memo.get(currIndex);
     }
