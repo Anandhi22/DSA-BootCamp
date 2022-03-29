332b959b7d4da47d540350ab4428822e687c3e06
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-03-29 21:51:48 +0530

    Create Number of Dice Rolls With Target Sum.java

---
+++ Dynamic-Programming/Number of Dice Rolls With Target Sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numRollsToTarget(int n, int k, int target) {
+        
+        return findTotalWays(n, k, target, new HashMap<>());
+    }
+    
+    public int findTotalWays(int n , int f, int target, HashMap<String, Integer> memo)
+    {
+        if(n==0 && target==0) return 1;
+        
+        if(n==0 && target>0) return 0;
+        
+        if(target<=0) return 0;
+        
+        String key = n + "-" + target;
+        
+        if(memo.containsKey(key)) return memo.get(key);
+        
+        int MOD = 1000000007;
+        
+        int ways = 0;
+        
+        for(int i = 1;i<=f;i++)
+        {
+            int totalWays = findTotalWays(n-1 , f, target-i, memo) % MOD ;
+            ways = ways%MOD;
+            ways = (totalWays + ways) % MOD;            
+        }
+        
+        memo.put(key, ways);
+        
+        return memo.get(key);
+        
+        
+    }
+}
