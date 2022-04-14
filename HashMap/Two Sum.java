d757bc32995e53538c87eca15f08c1f30c541295
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-04-14 21:55:01 +0530

    Update Two Sum.java

--- HashMap/Two Sum.java
+++ HashMap/Two Sum.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int[] ans = new int[2];
-        
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
+      HashMap<Integer, Integer> memo = new HashMap<>();
         
         for(int i = 0;i<nums.length;i++)
         {
             
             int key = target-nums[i];
-            if(map.containsKey(key)) 
-            {
-                ans[0] = map.get(key);
-                ans[1] = i;
-            }
+            if(memo.containsKey(key)) 
+             return new int[]{memo.get(key), i};
           else
             map.put(nums[i], i);
         }
-        return ans;
+      return new int[2];
     }
+    
 }
