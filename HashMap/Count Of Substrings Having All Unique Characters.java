3fc7ae82e285aa614c8d00902befc43ded1b8731
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-04-24 23:32:51 +0530

    Pepcoding

---
+++ HashMap/Count Of Substrings Having All Unique Characters.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static int solution(String str) {
+	
+	HashMap<Character, Integer> memo = new HashMap<>();
+	int release = 0;
+	int ans = 0;
+	
+	for(int acquire = 0;acquire<str.length();acquire++)
+	{
+	    char currentElement = str.charAt(acquire);
+	    
+	    while(release < acquire && memo.containsKey(currentElement))
+	    {
+	        char discardChar = str.charAt(release);
+	        memo.remove(discardChar);
+	        release += 1;
+	    }
+	    
+	    memo.put(currentElement, 1);
+	    ans += (acquire - release + 1);
+	}
+	
+	return ans;
+	
+	}
