ba85cd82fdfd1c27f212976f12833ed58fb84209
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-05-23 22:39:36 +0530

    Leetcode 78

---
+++ Recursion and Backtracking/Subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) 
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        findSubsets(nums, 0 ,new ArrayList<Integer>(), ans);
+        return ans;
+    }
+    
+    
+    public void findSubsets(int[] nums, int ind, ArrayList<Integer> currentSubset, List<List<Integer>> ans)
+    {
+        if(ind == nums.length)
+        {
+            ans.add(new ArrayList<Integer>(currentSubset));
+            return;
+        }
+        
+        currentSubset.add(nums[ind]);
+        
+        findSubsets(nums, ind+1,currentSubset, ans);
+        
+        currentSubset.remove(currentSubset.size()-1);
+        
+        findSubsets(nums, ind+1, currentSubset, ans);
+    }
+}
