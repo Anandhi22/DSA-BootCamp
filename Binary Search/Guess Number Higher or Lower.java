bacbc0851699ad0866b78fe1d2d242303a5a2772
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-07-06 01:36:08 +0530

    LC374. Guess Number Higher or Lower

---
+++ Binary Search/Guess Number Higher or Lower.java
@@ -0,0 +1,22 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        
+       int start = 1;
+        int end = n;
+     
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+           if(guess(mid)==0) return mid;
+            
+            else if(guess(mid) == 1)
+                start = mid+1;
+            
+            else end = mid-1;
+        }
+        
+        return -1;
+    }
+}
