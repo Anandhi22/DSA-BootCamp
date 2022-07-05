4aa41fa0a47ddb0ed4a23666019bcd962173577d
Anandhi Subramaniam <85279205+Anandhi22@users.noreply.github.com>
2022-07-06 00:48:42 +0530

    LC278. First Bad Version

---
+++ Binary Search/First Bad Version.java
@@ -0,0 +1,23 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int start = 1;
+        int end = n;
+        int firstBad = -1;
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if(isBadVersion(mid) == false)
+                start = mid+1;
+            else {
+                firstBad = mid;
+                end = mid-1;
+            }
+            
+        }
+        
+        return firstBad;
+    }
+}
